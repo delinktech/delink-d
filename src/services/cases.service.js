c3dfaa722ff4b544af085a85163d8e57e7f6e4ae
Delink <delinkdesigns@gmail.com>
2020-03-28 02:58:06 +0300

    added a service file to fetch data from api

---
+++ src/services/cases.service.js
@@ -0,0 +1,38 @@
+const axios = require('axios');
+import config from "../../config.json";
+
+/**
+ * Function get all countries data from the api
+ */
+export function getCasesByCountry() {
+  return new Promise((resolve, reject) => {
+    console.log(config);
+    axios
+      .get(config.RAPIDAPI.URL + "/cases_by_country.php", {
+        "headers": {
+          "x-rapidapi-host": config.RAPIDAPI.HOST,
+          "x-rapidapi-key": config.RAPIDAPI.API_KEY
+        }
+      })
+      .then(response => resolve(response.data))
+      .catch(error => reject(error));
+  });
+
+}
+
+/**
+ * Function get world data from the api
+ */
+export function getWorldStats() {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(config.RAPIDAPI.URL + "/worldstat.php", {
+        "headers": {
+          "x-rapidapi-host": config.RAPIDAPI.HOST,
+          "x-rapidapi-key": config.RAPIDAPI.API_KEY
+        }
+      })
+      .then(response => resolve(response.data))
+      .catch(error => reject(error));
+  });
+}
