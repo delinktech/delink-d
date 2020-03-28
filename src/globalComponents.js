4e21b6e785e282bf382b42c8d497e7febeb93b60
Delink <delinkdesigns@gmail.com>
2020-03-28 03:38:25 +0300

    remove default code

--- src/globalComponents.js
+++ src/globalComponents.js
@@ -1,13 +1,9 @@
-import DropDown from "./components/Dropdown.vue";
-
 /**
  * You can register global components here and use them as a plugin in your main Vue instance
  */
 
 const GlobalComponents = {
-  install(Vue) {
-    Vue.component("drop-down", DropDown);
-  }
+  install(Vue) { }
 };
 
 export default GlobalComponents;
