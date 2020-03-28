15eda5c4df2e264e657ecb471596c7854c1ec669
Delink <delinkdesigns@gmail.com>
2020-03-28 03:55:04 +0300

    comment out to used later code

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -14,9 +14,9 @@
           </template>
 
           <template slot="footer">
-            <div class="stats">
+            <!-- <div class="stats">
               <md-icon>date_range</md-icon>Last 24 Hours
-            </div>
+            </div> -->
           </template>
         </stats-card>
       </div>
@@ -32,9 +32,9 @@
           </template>
 
           <template slot="footer">
-            <div class="stats">
+            <!-- <div class="stats">
               <md-icon>local_offer</md-icon>Tracked from Github
-            </div>
+            </div> -->
           </template>
         </stats-card>
       </div>
@@ -50,9 +50,9 @@
           </template>
 
           <template slot="footer">
-            <div class="stats">
+            <!-- <div class="stats">
               <md-icon>update</md-icon>Just Updated
-            </div>
+            </div> -->
           </template>
         </stats-card>
       </div>
@@ -68,10 +68,10 @@
           </template>
 
           <template slot="footer">
-            <div class="stats">
+            <!-- <div class="stats">
               <md-icon class="text-danger">warning</md-icon>
               <a href="#pablo">Get More Space...</a>
-            </div>
+            </div> -->
           </template>
         </stats-card>
       </div>
@@ -90,13 +90,12 @@
       </div>
 
       <!-- row 3 global data on charts -->
-      <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33">
+      <!-- <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33">
         <chart-card
           :chart-data="dailySalesChart.data"
           :chart-options="dailySalesChart.options"
           :chart-type="'Line'"
-          data-background-color="blue"
-        >
+          data-background-color="blue">
           <template slot="content">
             <h4 class="title">Daily Sales</h4>
             <p class="category">
@@ -120,8 +119,7 @@
           :chart-options="emailsSubscriptionChart.options"
           :chart-responsive-options="emailsSubscriptionChart.responsiveOptions"
           :chart-type="'Bar'"
-          data-background-color="red"
-        >
+          data-background-color="red">
           <template slot="content">
             <h4 class="title">Email Subscription</h4>
             <p class="category">Last Campaign Performance</p>
@@ -139,8 +137,7 @@
           :chart-data="dataCompletedTasksChart.data"
           :chart-options="dataCompletedTasksChart.options"
           :chart-type="'Line'"
-          data-background-color="green"
-        >
+          data-background-color="green">
           <template slot="content">
             <h4 class="title">Completed Tasks</h4>
             <p class="category">Last Campaign Performance</p>
@@ -152,7 +149,7 @@
             </div>
           </template>
         </chart-card>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
@@ -168,7 +165,7 @@ import {
 export default {
   components: {
     StatsCard,
-    ChartCard,
+    // ChartCard,
     OrderedTable
   },
   data() {
@@ -181,90 +178,90 @@ export default {
         new_deaths: null,
         statistic_taken_at: null
       },
-      dailySalesChart: {
-        data: {
-          labels: ["M", "T", "W", "T", "F", "S", "S"],
-          series: [[12, 17, 7, 17, 23, 18, 38]]
-        },
-        options: {
-          lineSmooth: this.$Chartist.Interpolation.cardinal({
-            tension: 0
-          }),
-          low: 0,
-          high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-          chartPadding: {
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: 0
-          }
-        }
-      },
-      dataCompletedTasksChart: {
-        data: {
-          labels: ["12am", "3pm", "6pm", "9pm", "12pm", "3am", "6am", "9am"],
-          series: [[230, 750, 450, 300, 280, 240, 200, 190]]
-        },
+      // dailySalesChart: {
+      //   data: {
+      //     labels: ["M", "T", "W", "T", "F", "S", "S"],
+      //     series: [[12, 17, 7, 17, 23, 18, 38]]
+      //   },
+      //   options: {
+      //     lineSmooth: this.$Chartist.Interpolation.cardinal({
+      //       tension: 0
+      //     }),
+      //     low: 0,
+      //     high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+      //     chartPadding: {
+      //       top: 0,
+      //       right: 0,
+      //       bottom: 0,
+      //       left: 0
+      //     }
+      //   }
+      // },
+      // dataCompletedTasksChart: {
+      //   data: {
+      //     labels: ["12am", "3pm", "6pm", "9pm", "12pm", "3am", "6am", "9am"],
+      //     series: [[230, 750, 450, 300, 280, 240, 200, 190]]
+      //   },
 
-        options: {
-          lineSmooth: this.$Chartist.Interpolation.cardinal({
-            tension: 0
-          }),
-          low: 0,
-          high: 1000, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-          chartPadding: {
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: 0
-          }
-        }
-      },
-      emailsSubscriptionChart: {
-        data: {
-          labels: [
-            "Ja",
-            "Fe",
-            "Ma",
-            "Ap",
-            "Mai",
-            "Ju",
-            "Jul",
-            "Au",
-            "Se",
-            "Oc",
-            "No",
-            "De"
-          ],
-          series: [[542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895]]
-        },
-        options: {
-          axisX: {
-            showGrid: false
-          },
-          low: 0,
-          high: 1000,
-          chartPadding: {
-            top: 0,
-            right: 5,
-            bottom: 0,
-            left: 0
-          }
-        },
-        responsiveOptions: [
-          [
-            "screen and (max-width: 640px)",
-            {
-              seriesBarDistance: 5,
-              axisX: {
-                labelInterpolationFnc: function(value) {
-                  return value[0];
-                }
-              }
-            }
-          ]
-        ]
-      }
+      //   options: {
+      //     lineSmooth: this.$Chartist.Interpolation.cardinal({
+      //       tension: 0
+      //     }),
+      //     low: 0,
+      //     high: 1000, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+      //     chartPadding: {
+      //       top: 0,
+      //       right: 0,
+      //       bottom: 0,
+      //       left: 0
+      //     }
+      //   }
+      // },
+      // emailsSubscriptionChart: {
+      //   data: {
+      //     labels: [
+      //       "Ja",
+      //       "Fe",
+      //       "Ma",
+      //       "Ap",
+      //       "Mai",
+      //       "Ju",
+      //       "Jul",
+      //       "Au",
+      //       "Se",
+      //       "Oc",
+      //       "No",
+      //       "De"
+      //     ],
+      //     series: [[542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895]]
+      //   },
+      //   options: {
+      //     axisX: {
+      //       showGrid: false
+      //     },
+      //     low: 0,
+      //     high: 1000,
+      //     chartPadding: {
+      //       top: 0,
+      //       right: 5,
+      //       bottom: 0,
+      //       left: 0
+      //     }
+      //   },
+      //   responsiveOptions: [
+      //     [
+      //       "screen and (max-width: 640px)",
+      //       {
+      //         seriesBarDistance: 5,
+      //         axisX: {
+      //           labelInterpolationFnc: function(value) {
+      //             return value[0];
+      //           }
+      //         }
+      //       }
+      //     ]
+      //   ]
+      // }
     };
   },
   created() {
